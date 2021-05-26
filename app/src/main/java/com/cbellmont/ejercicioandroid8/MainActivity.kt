a4aa6f1b7dbd24183aabf84d1342dc0a5e553ee6
Carlos <carlostena@centroafuera.es>
2021-05-26 13:59:51 +0200

    updated to 1.5.10

--- app/src/main/java/com/cbellmont/ejercicioandroid8/MainActivity.kt
+++ app/src/main/java/com/cbellmont/ejercicioandroid8/MainActivity.kt
@@ -4,45 +4,45 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
-import android.view.View
 import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
-import kotlinx.android.synthetic.main.activity_main.*
+import com.cbellmont.ejercicioandroid8.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         addListenerToButton()
         addListenerToEditText()
         addListenerToTextView()
     }
 
     private fun addListenerToTextView() {
-        twContent.setOnLongClickListener {
+        binding.twContent.setOnLongClickListener {
             Toast.makeText(this, "Transformando texto a mayusculas", Toast.LENGTH_LONG).show()
-            etContent.setText(etContent.text.toString().toUpperCase(Locale.getDefault()))
+            binding.etContent.setText(binding.etContent.text.toString().uppercase(Locale.getDefault()))
             true
         }
     }
 
     private fun addListenerToButton() {
-        bConvert.setOnClickListener {
-            if(twContent.text.isEmpty())
+        binding.bConvert.setOnClickListener {
+            if(binding.twContent.text.isEmpty())
                 Toast.makeText(this, "Escribe algo primero", Toast.LENGTH_LONG).show()
             else
-                Toast.makeText(this, "Hay ${twContent.text.length} caracteres escritos", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Hay ${binding.twContent.text.length} caracteres escritos", Toast.LENGTH_LONG).show()
         }
     }
 
 
     private fun addListenerToEditText() {
-        etContent.addTextChangedListener( object : TextWatcher {
+        binding.etContent.addTextChangedListener( object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-                twContent.text = etContent.text
+                binding.twContent.text = binding.etContent.text
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
